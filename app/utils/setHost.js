c78a2a5939164e977e318f12278da23638628c14
左文祺 <zuowenqi@yy.com>
2018-12-10 10:56:01 +0800

    修复设置host时加的注释导致host不生效的问题

--- app/utils/setHost.js
+++ app/utils/setHost.js
@@ -7,7 +7,7 @@ module.exports = function (ip, domain) {
             console.info(`-----------host ${domain} 已经存在-----------`)
             tips = `changed`;
         }
-        host.set(ip, domain, "local-dev", true, "本地开发环境---yeoman")
+        host.set(ip, domain, "local-dev", true);
         console.info(`-----------host ${domain} ${tips}-----------`);
     } catch (e) {
         console.warn(e);
